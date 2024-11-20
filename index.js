c715294552c597fdac69975d46c6a4f9bf145dd0
Tito-Rollis <ouvalres@gmail.com>
2024-11-20 17:46:41 +0700

    all tasks is clear

--- index.js
+++ index.js
@@ -3,27 +3,24 @@ const successText = document.getElementById('success-text');
 const errorText = document.getElementById('error-text');
 const submitButton = document.getElementById('btn');
 
-/*
-    a) in case we try to submit email harry@potter.com - the form should display error
-    message
-    b) in case we try to submit email albus@dumbledore.com the form should display
-    success message
-*/
-
 let getInputValue = '';
 
+// get the value from input text then put in to getInputValue variable
 inputValue.addEventListener('change', (e) => (getInputValue = e.target.value));
 
+// add submit handler for the input value including the conditional logic
 submitButton.addEventListener('click', () => {
     switch (getInputValue) {
         case 'harry@potter.com':
             errorText.style.display = 'block';
             successText.style.display = 'none';
+            inputValue.style.border = '1px solid #FDA29B';
             break;
 
         case 'albus@dumbledore.com':
             successText.style.display = 'block';
             errorText.style.display = 'none';
+            inputValue.style.border = '1px solid #d1d5db';
             break;
     }
 });
